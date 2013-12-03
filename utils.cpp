55cdbe1fe5c8cae9d3c91a5d22998698839e2eaa
Alex Addy <alex.addy@gmail.com>
2013-12-03 11:25:23 -0500

    Split out file utils.hpp, and did some function work.

--- utils.cpp
+++ utils.cpp
@@ -1,15 +1,44 @@
 
-#ifndef UTILS_HPP
-#define UTILS_HPP
-
+#include "utils.hpp"
 #include <string>
 #include <vector>
 #include <fstream>
+#include <map>
+#include <stdlib>
+#include <cassert>
 
-const int Q0 = 0.5 // must be in range [0,1], a lower q favors exploration over exploitation
-const int ALPHA = 0.5 // must be in range [0,1], pheremone decay factor
+std::vector<std::vector<int> > read_the_file(std::string s)
+{
+	std::ifstream f(s);
+	std::string key, val;
+	std::map<std::string, std::string> kv;
+	std::vector<int> simple_dists;
 
-std::vector<std::vector<int> > read_the_file(std::string s);
+	cin >> key;
+	while (key and key != "EOF")
+	{
+		if (key == "EDGE_WEIGHT_SECTION")
+		{
+			cin >> val;
+			int v = atoi(val.c_str())
+			while (v and val != "EOF")
+			{
+				simple_dists.push_back(v);
+				cin >> val; v = atoi(val);
+			}
+		}
+		else
+		{
+			cin >> val;
+			kv[key] = val;
+		}
+		cin >> key;
+	}
+
+	assert(simple_dists.size() > 0)
+
+	// react to the given data and fill in the array to be returned
+}
 std::vector<std::vector<double> > setup_pheromones(std::vector<std::vector<int>> p);
 
 // euclidean distance
@@ -28,6 +57,6 @@ double eq2(vector<vector<int>>& dist, vector<vector<double>>& pheromones);
 double eq3(double old_pheromone);
 
 // returns the new pheromone level to update the current global best path
-double eq4(double old_pheromone, int lowest_path_cost);
+double eq4(double old_pheromone, int total_path_cost);
 
 #endif
---
+++ utils.hpp
@@ -0,0 +1,30 @@
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+const int Q0 = 0.5 // must be in range [0,1], a lower q favors exploration over exploitation
+const int ALPHA = 0.5 // must be in range [0,1], pheremone decay factor
+
+std::vector<std::vector<int> > read_the_file(std::string s);
+std::vector<std::vector<double> > setup_pheromones(std::vector<std::vector<int>> p);
+
+// euclidean distance
+double distance(std::pair<double, double>, std::pair<double, double>);
+double distance(std::pair<int, int>, std::pair<int, int>);
+double distance(int, int, int, int);
+double distance(double, double, double, double);
+
+// returns the index of the path to take, will call eq2 if needed
+std::pair<int,int> eq1(vector<vector<int>>& dist, vector<vector<double>>& pheromones);
+
+// returns the new pheromone level
+double eq3(double old_pheromone);
+
+// returns the new pheromone level to update the current global best path
+double eq4(double old_pheromone, int total_path_cost);
+
+#endif
